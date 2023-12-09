8b7d87830488d1695eeb5237e11bc774c0c578c3
Leonardo Mora <Leonardo_Mora@Dell.com>
2023-12-08 21:39:16 -0600

    Specifying file types.

--- configs/null-ls.lua
+++ configs/null-ls.lua
@@ -1,12 +1,12 @@
 local null_ls = require "null-ls"
 
 local sources = {
-  null_ls.builtins.formatting.black,
+  null_ls.builtins.formatting.black.with({ filetypes = { "python" } }),
   -- null_ls.builtins.diagnostics.mypy,
-  null_ls.builtins.diagnostics.ruff,
+  null_ls.builtins.diagnostics.ruff.with({ filetypes = { "python" } }),
   -- null_ls.builtins.diagnostics.shellcheck,
-  null_ls.builtins.formatting.shfmt,
-  null_ls.builtins.formatting.prettier,
+  null_ls.builtins.formatting.shfmt.with({ filetypes = { "sh", "zsh" } }),
+  -- null_ls.builtins.formatting.prettier,
   null_ls.builtins.formatting.stylua,
 }
 
