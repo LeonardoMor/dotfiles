4139e80be856d5bd791b06e843062722931954b8
Leonardo Mora <Leonardo_Mora@Dell.com>
2023-12-10 22:20:05 -0600

    Format when saving

--- configs/null-ls.lua
+++ configs/null-ls.lua
@@ -1,16 +1,31 @@
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 local null_ls = require "null-ls"
 
-local sources = {
-  null_ls.builtins.formatting.black.with({ filetypes = { "python" } }),
-  -- null_ls.builtins.diagnostics.mypy,
-  null_ls.builtins.diagnostics.ruff.with({ filetypes = { "python" } }),
-  -- null_ls.builtins.diagnostics.shellcheck,
-  null_ls.builtins.formatting.shfmt.with({ filetypes = { "sh", "zsh" } }),
-  -- null_ls.builtins.formatting.prettier,
-  null_ls.builtins.formatting.stylua,
+local opts = {
+  sources = {
+    null_ls.builtins.formatting.black.with({ filetypes = { "python" } }),
+    -- null_ls.builtins.diagnostics.mypy,
+    null_ls.builtins.diagnostics.ruff.with({ filetypes = { "python" } }),
+    -- null_ls.builtins.diagnostics.shellcheck,
+    null_ls.builtins.formatting.shfmt.with({ filetypes = { "sh", "zsh" } }),
+    -- null_ls.builtins.formatting.prettier,
+    null_ls.builtins.formatting.stylua,
+  },
+  on_attach = function(client, bufnr)
+    if client.supports_method("textDocument/formatting") then
+      vim.api.nvim_clear_autocmds({
+        group = augroup,
+        buffer = bufnr,
+      })
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.buf.format({ bufnr = bufnr })
+        end,
+      })
+    end
+  end,
 }
 
-null_ls.setup {
-  debug = true,
-  sources = sources,
-}
+null_ls.setup(opts)
