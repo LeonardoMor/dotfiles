00052f36917060ea3d00d0c93c14e09d78ac3a5e
Leonardo Mora <Leonardo_Mora@Dell.com>
2023-12-16 19:31:54 -0600

    Proper none-ls config

--- configs/none-ls.lua
+++ configs/none-ls.lua
@@ -1,5 +1,5 @@
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-local none_ls = require "none-ls"
+local none_ls = require "null-ls"
 
 local opts = {
   sources = {
@@ -21,7 +21,7 @@ local opts = {
         group = augroup,
         buffer = bufnr,
         callback = function()
-          vim.lsp.buf.format { bufnr = bufnr }
+          vim.lsp.buf.format({ async = false })
         end,
       })
     end
