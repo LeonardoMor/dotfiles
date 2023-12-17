41e4b1d4c1ed71373dd486920e2426b23e7ea780
Leonardo Mora <Leonardo_Mora@Dell.com>
2023-12-16 19:14:26 -0600

    Migrating to none-ls

--- configs/none-ls.lua
+++ configs/none-ls.lua
@@ -1,31 +1,31 @@
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-local null_ls = require "null-ls"
+local none_ls = require "none-ls"
 
 local opts = {
   sources = {
-    null_ls.builtins.formatting.black.with({ filetypes = { "python" } }),
-    -- null_ls.builtins.diagnostics.mypy,
-    null_ls.builtins.diagnostics.ruff.with({ filetypes = { "python" } }),
-    -- null_ls.builtins.diagnostics.shellcheck,
-    null_ls.builtins.formatting.shfmt.with({ filetypes = { "sh", "zsh" } }),
-    -- null_ls.builtins.formatting.prettier,
-    null_ls.builtins.formatting.stylua,
+    none_ls.builtins.formatting.black.with { filetypes = { "python" } },
+    -- none_ls.builtins.diagnostics.mypy,
+    none_ls.builtins.diagnostics.ruff.with { filetypes = { "python" } },
+    -- none_ls.builtins.diagnostics.shellcheck,
+    none_ls.builtins.formatting.shfmt.with { filetypes = { "sh", "zsh" } },
+    -- none_ls.builtins.formatting.prettier,
+    none_ls.builtins.formatting.stylua,
   },
   on_attach = function(client, bufnr)
-    if client.supports_method("textDocument/formatting") then
-      vim.api.nvim_clear_autocmds({
+    if client.supports_method "textDocument/formatting" then
+      vim.api.nvim_clear_autocmds {
         group = augroup,
         buffer = bufnr,
-      })
+      }
       vim.api.nvim_create_autocmd("BufWritePre", {
         group = augroup,
         buffer = bufnr,
         callback = function()
-          vim.lsp.buf.format({ bufnr = bufnr })
+          vim.lsp.buf.format { bufnr = bufnr }
         end,
       })
     end
   end,
 }
 
-null_ls.setup(opts)
+none_ls.setup(opts)
