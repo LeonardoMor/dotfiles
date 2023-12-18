18121b3542009412e535822939571cc1efd12821
Leonardo Mora <leonardo_mora@dell.com>
2023-12-17 19:34:04 -0600

    Proper search highlight behavior

--- init.lua
+++ init.lua
@@ -1,2 +1,6 @@
-# Relative line numbers on by default
+-- Relative line numbers on by default
 vim.opt.relativenumber = true
+
+-- Don't keep search highlight, but highlight as you type
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
