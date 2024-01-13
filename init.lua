a199ace4be2bd431b929658f1dada0b9e8e61493
Leonardo Mora <leonardo_mora@dell.com>
2024-01-13 17:23:28 -0600

    Briefly highlighting yanked text

--- init.lua
+++ init.lua
@@ -1,8 +1,16 @@
 -- Relative line numbers on by default
 vim.opt.relativenumber = true
-
 -- Don't keep search highlight, but highlight as you type
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 -- Disable Codeium default bindings
 vim.g.codeium_disable_bindings = 1
+-- Briefly highlight yanked text
+vim.api.nvim_create_autocmd ('TextYankPost', {
+    group = vim.api.nvim_create_augroup ('highlight_yank', {}),
+    desc = 'Hightlight selection on yank',
+    pattern = '*',
+    callback = function ()
+      vim.highlight.on_yank { higroup = 'IncSearch', timeout = 125 }
+    end,
+  })  
