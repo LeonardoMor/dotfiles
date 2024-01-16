157c7953ffde345b9adc020429a292531494f552
Leonardo Mora <leonardo_mora@dell.com>
2024-01-15 21:56:44 -0600

    Adding parial completions https://github.com/Exafunction/codeium.vim/issues/27

--- mappings.lua
+++ mappings.lua
@@ -1,5 +1,6 @@
 local M = {}
 local fn = vim.fn
+local api = vim.api
 
 M.general = {
   n = {
@@ -83,6 +84,47 @@ M.codeium = {
         return fn["codeium#Clear"]()
       end,
       "Clear completion",
+      opts = { expr = true },
+    },
+    ["<C-Right>"] = {
+      function()
+        local fullCompletion =
+          api.nvim_eval "b:_codeium_completions.items[b:_codeium_completions.index].completionParts[0].text"
+        local cursor = api.nvim_win_get_cursor(0)
+        local line = api.nvim_get_current_line()
+        local completion = string.match(fullCompletion, "[ ,;.]*[^ ,;.]+")
+        vim.defer_fn(function()
+          if string.match(completion, "^\t") then
+            api.nvim_buf_set_lines(0, cursor[1], cursor[1], true, { completion })
+            api.nvim_win_set_cursor(0, { cursor[1] + 1, #completion })
+          else
+            local nline = line:sub(0, cursor[2]) .. completion .. line:sub(cursor[2] + 1)
+            api.nvim_set_current_line(nline)
+            api.nvim_win_set_cursor(0, { cursor[1], cursor[2] + #completion })
+          end
+        end, 0)
+      end,
+      "Accept next word in completion",
+      opts = { expr = true },
+    },
+    ["<Right>"] = {
+      function()
+        local fullCompletion =
+          api.nvim_eval "b:_codeium_completions.items[b:_codeium_completions.index].completionParts[0].text"
+        local cursor = api.nvim_win_get_cursor(0)
+        local line = api.nvim_get_current_line()
+        local completion = string.gsub(fullCompletion, "\n.*$", "")
+        if completion ~= "" then
+          vim.defer_fn(function()
+            local nline = line:sub(0, cursor[2]) .. completion .. line:sub(cursor[2] + 1)
+            api.nvim_set_current_line(nline)
+            api.nvim_win_set_cursor(0, { cursor[1], cursor[2] + #completion })
+            print("pre enter " .. vim.inspect(cursor))
+            api.nvim_feedkeys("\n", "i", true)
+          end, 0)
+        end
+      end,
+      "Accept next line in completion",
       opts = { expr = true },
     },
   },
