107b60af8e162826b79ebbb90ee8ee8faa2918f4
Leonardo Mora Castro <leomc145@gmail.com>
2024-11-18 12:10:10 -0600

    refactor: determine brew executable even if already installed

--- .chezmoiscripts/darwin/.prereqs.sh
+++ .chezmoiscripts/darwin/.prereqs.sh
@@ -4,15 +4,15 @@ set -e
 
 brew --version >/dev/null 2>&1 || {
 	curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
-
-	if /opt/homebrew/bin/brew --version >/dev/null 2>&1; then
-		homebrew='/opt/homebrew/bin/brew'
-	elif /usr/local/bin/brew --version >/dev/null 2>&1; then
-		homebrew='/usr/local/bin/brew'
-	elif /home/linuxbrew/.linuxbrew/bin/brew --version >/dev/null 2>&1; then
-		homebrew='/home/linuxbrew/.linuxbrew/bin/brew'
-	fi
 }
+
+if /opt/homebrew/bin/brew --version >/dev/null 2>&1; then
+	homebrew='/opt/homebrew/bin/brew'
+elif /usr/local/bin/brew --version >/dev/null 2>&1; then
+	homebrew='/usr/local/bin/brew'
+elif /home/linuxbrew/.linuxbrew/bin/brew --version >/dev/null 2>&1; then
+	homebrew='/home/linuxbrew/.linuxbrew/bin/brew'
+fi
 
 # Will manage brew packages with Homebrew-file
 brew-file --version >/dev/null 2>&1 || "$homebrew" install rcmdnk/file/brew-file
