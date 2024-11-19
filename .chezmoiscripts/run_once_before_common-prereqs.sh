103b9f9e8885a6c70d17049c0baf5b6b5b23a2fc
Leonardo Mora Castro <leomc145@gmail.com>
2024-11-19 15:29:45 -0600

    refactor: check before installing

--- .chezmoiscripts/run_once_before_common-prereqs.sh
+++ .chezmoiscripts/run_once_before_common-prereqs.sh
@@ -4,4 +4,4 @@ XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-"${HOME}/.config"}"
 
 # Install Tmux Plugin Manager
 # You still need to open tmux and do prefix + I
-git clone https://github.com/tmux-plugins/tpm "${XDG_CONFIG_HOME}/tmux/plugins/tpm"
+[[ -f "${XDG_CONFIG_HOME}/tmux/plugins/tpm/tpm" ]] || git clone https://github.com/tmux-plugins/tpm "${XDG_CONFIG_HOME}/tmux/plugins/tpm"
