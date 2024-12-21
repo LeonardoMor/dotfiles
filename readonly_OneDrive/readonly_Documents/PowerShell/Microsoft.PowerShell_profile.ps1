aa506da94ae1853629d5c71dbce070ce948fd25c
Leonardo Mora Castro <leomc145@gmail.com>
2024-12-20 18:57:15 -0600

    refactor(pwsh): match other shells config

--- readonly_OneDrive/readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ readonly_OneDrive/readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -2,7 +2,7 @@ Invoke-Expression (&starship init powershell)
 
 # Keybindings for completion
 Set-PSReadLineKeyHandler -Chord "Alt+l" -Function ForwardWord
-Set-PSReadLineKeyHandler -Chord "Ctrl+f" -Function ForwardChar
+Set-PSReadLineKeyHandler -Chord "Alt+h" -Function ForwardChar
 
 # Command completions
 . C:\Users\leomc\AppData\Local\starship\starship_completions.ps1
