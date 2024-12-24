0ff8f0e245accbc228655928b90eb5a6ca43e20e
Leonardo Mora Castro <leomc145@gmail.com>
2024-12-24 11:34:08 -0600

    refactor(windows): start by upgrading winget

--- .chezmoiscripts/windows/.prereqs.ps1
+++ .chezmoiscripts/windows/.prereqs.ps1
@@ -1,1 +1,22 @@
-winget upgrade winget
+function Compare-Version {
+    param (
+        [string]$Version1,
+        [string]$Version2
+    )
+
+    $Version1 = $Version1.TrimStart('v')
+    $Version2 = $Version2.TrimStart('v')
+
+    $v1 = [Version]$Version1
+    $v2 = [Version]$Version2
+
+    return [int]($v1.CompareTo($v2))
+}
+
+$targetVersion = "1.9.25200"
+$currentVersion = winget --version
+
+if (Compare-Version $currentVersion $targetVersion -lt 0) {
+    Write-Host "Upgrading winget to the latest version..."
+    winget upgrade --id Microsoft.DesktopAppInstaller --silent
+}
