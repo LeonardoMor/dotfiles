f450cc0b39e9d8a6eb07c36014ffc9a369936cfa
Leonardo Mora Castro <leomc145@gmail.com>
2025-01-10 19:16:00 -0600

    refactor(nvim): work vault only on work pc

--- .chezmoitemplates/nvim/lua/custom/utils.lua
+++ .chezmoitemplates/nvim/lua/custom/utils.lua
@@ -4,10 +4,12 @@ return {
         name = 'Codice',
         path = {{ joinPath .DocumentsDir "Codice" | squote }},
       },
+{{- if eq .chezmoi.hostname .work_host }}
       {
         name = 'Meta Work',
         path = {{ joinPath .DocumentsDir "Meta_Work" | squote }},
       },
+{{- end }}
       {
         name = 'Splunk Cloud Support',
         path = {{ joinPath .DocumentsDir "Splunk_Cloud_Support" | squote }},
