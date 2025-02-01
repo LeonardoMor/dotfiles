734647f5abef2684a69266ccacb897a2222ad187
Leonardo Mora Castro <leomc145@gmail.com>
2025-01-31 19:54:52 -0600

    feat(prereqs): on Windows, install JetBrains Nerd Font
    As a pre-requisite.

--- .chezmoiscripts/windows/.prereqs.ps1
+++ .chezmoiscripts/windows/.prereqs.ps1
@@ -1,4 +1,5 @@
-function Compare-Version {
+function Compare-Version
+{
     param (
         [string]$Version1,
         [string]$Version2
@@ -13,10 +14,25 @@ function Compare-Version {
     return [int]($v1.CompareTo($v2))
 }
 
+function Invoke-AsAdmin
+{
+    param (
+        [string]$ScriptName
+    )
+
+    $ScriptPath = Join-Path -Path $PSScriptRoot -ChildPath $ScriptName
+
+    Start-Process -FilePath "powershell.exe" -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$ScriptPath`"" -Verb RunAs
+}
+
 $targetVersion = "1.9.25200"
 $currentVersion = winget --version
 
-if (Compare-Version $currentVersion $targetVersion -lt 0) {
+if (Compare-Version $currentVersion $targetVersion -lt 0)
+{
     Write-Host "Upgrading winget to the latest version..."
     winget install -e --accept-source-agreements --accept-package-agreements Microsoft.AppInstaller
 }
+
+# Install JetBrains Nerd Font
+Invoke-AsAdmin "Install-JetBrainsMonoNerdFont.ps1"
