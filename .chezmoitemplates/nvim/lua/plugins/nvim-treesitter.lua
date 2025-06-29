f603e073db1f811c078ff9f597d7cf9906c542dd
Leonardo Mora Castro <lmoracas@cisco.com>
2025-06-28 21:57:06 -0600

    fix(nvim-treesitter.nvim): point to correct module
    For configs.

--- .chezmoitemplates/nvim/lua/plugins/nvim-treesitter.lua
+++ .chezmoitemplates/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,6 +1,7 @@
 return { -- Highlight, edit, and navigate code
   'nvim-treesitter/nvim-treesitter',
   build = ':TSUpdate',
+  main = 'nvim-treesitter.configs',
   opts = {
     ensure_installed = {
       'bash',
