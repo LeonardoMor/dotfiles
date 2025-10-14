1f06cbdd9fbb27eba669eef8147718864289194b
Leonardo Mora Castro <leomc145@gmail.com>
2025-10-14 12:26:39 -0600

    fix(nvm): working nvm on Arch Linux
    Finally.
    
    Squashed commit of the following:
    
    commit 8c895b10e59c6dd503ac1e89d954f976f2955c07
    Author: Leonardo Mora Castro <leomc145@gmail.com>
    Date:   Tue Oct 14 12:25:44 2025 -0600
    
        feat(linux): add nvm
    
    commit 61d8c1f2689e57613b99c785db336bf028ec2058
    Author: Leonardo Mora Castro <leomc145@gmail.com>
    Date:   Tue Oct 14 12:18:58 2025 -0600
    
        refactor: better function name
    
    commit 82b4b6e0aa6b66b4c5af2d6ca1ba04f61805e78e
    Author: Leonardo Mora Castro <leomc145@gmail.com>
    Date:   Tue Oct 14 12:17:55 2025 -0600
    
        refactor: take advantage of change_dir
    
    commit 299cb09488af77e42b2a09629d783ac88d4ffc2e
    Author: Leonardo Mora Castro <leomc145@gmail.com>
    Date:   Tue Oct 14 12:14:15 2025 -0600
    
        refactor: remove redundant export
    
    commit 7b5dcf0d87b392a37f5543402b208a3fa58f6633
    Author: Leonardo Mora Castro <leomc145@gmail.com>
    Date:   Tue Oct 14 12:13:11 2025 -0600
    
        feat: get useful functions from .utils.sh
    
    commit 94bfcfbf5a1837756633096d0174dc1a6a69d766
    Author: Leonardo Mora Castro <leomc145@gmail.com>
    Date:   Tue Oct 14 12:12:31 2025 -0600
    
        refactor(.utils.sh): improve code
    
    commit 338fcb3152e58a6b3e386a28257c880fa3add280
    Author: Leonardo Mora Castro <leomc145@gmail.com>
    Date:   Tue Oct 14 12:04:12 2025 -0600
    
        refactor: handle error yourself
    
    commit 7c570b15acdf2ed3f271e73663d33189ce827af0
    Author: Leonardo Mora Castro <leomc145@gmail.com>
    Date:   Tue Oct 14 12:02:26 2025 -0600
    
        refactor(nvm): configure nvm on .profile
    
        Consolidate config in one place. Most setup is for macOS and Linux setup
        is simple.
    
        See: https://gitlab.archlinux.org/archlinux/packaging/packages/nvm/-/blob/main/nvm.install?ref_type=heads
    
    commit 19b9f1a567e674d01e421d35667e4c5862dc88db
    Author: Leonardo Mora Castro <leomc145@gmail.com>
    Date:   Tue Oct 14 10:19:59 2025 -0600
    
        refactor(nvm): remove from Linux

--- .chezmoiscripts/.utils.sh
+++ .chezmoiscripts/.utils.sh
@@ -1,28 +1,16 @@
 #!/usr/bin/env bash
 
-die() {
+emit() {
     case "$1" in
-        i)
-            printf 'INFO: %s\n' "$2" >&2
-            ;;
-        e)
-            printf 'ERROR: %s\n' "$2" >&2
-            ;;
-        w)
-            printf 'WARNING: %s\n' "$2" >&2
-            ;;
-        f)
-            printf 'FATAL: %s\n' "$2" >&2
-            ;;
-        *)
-            die e "Invalid option or format"
-            ;;
+        i) printf 'INFO: %s\n' "$2" >&2 ;;
+        e) printf 'ERROR: %s\n' "$2" >&2 ;;
+        w) printf 'WARNING: %s\n' "$2" >&2 ;;
+        f) printf 'FATAL: %s\n' "$2" >&2 ;;
+        *) emit e "Invalid option or format" ;;
     esac
+    [[ -z $3 ]] || exit "$3"
 }
 
-change_dir() {
-    cd "$1" || {
-        die e "Failed to change $1"
-        exit 1
-    }
+change-dir() {
+    cd "$1" || emit f "Failed to change $1" 1
 }
