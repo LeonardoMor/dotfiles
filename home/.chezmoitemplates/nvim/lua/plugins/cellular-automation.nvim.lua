a6511ca8baa12818a2daefd7e5b33ca01d98e929
Leonardo Mora Castro <lmoracas@cisco.com>
2025-11-05 17:20:29 -0600

    feat(neovim): add cellular-automation.nvim

---
+++ home/.chezmoitemplates/nvim/lua/plugins/cellular-automation.nvim.lua
@@ -0,0 +1,10 @@
+    {
+        'eandrju/cellular-automaton.nvim',
+        cmd = 'CellularAutomaton',
+        keys = {
+            { '<leader>fml', '<cmd>CellularAutomaton make_it_rain<cr>' }
+        }
+    }
+{{- /*
+vim: filetype=lua.gotmpl
+*/ -}}
